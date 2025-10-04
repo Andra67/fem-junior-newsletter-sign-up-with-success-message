3edf95611a773e2d408b744c80a4a0cb53788279
Julien Andra <julien.andra01@gmail.com>
2025-10-04 23:16:18 +0700

    finalizing

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const content = document.querySelector(".content");
-const sucessMsg = document.querySelector(".success-message");
+const successMsg = document.querySelector(".success-message");
 const form = document.querySelector(".signup-form");
 const dismissBtn = document.querySelector(".dismiss-btn");
+const userEmail = document.getElementById("user-email");
+const emailInput = document.getElementById("email");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  content.style.display = "none";
-  sucessMsg.style.display = "block";
+
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (emailPattern.test(emailInput.value)) {
+    userEmail.textContent = emailInput.value;
+    content.style.display = "none";
+    successMsg.style.display = "block";
+    emailInput.classList.remove("email-error");
+  } else {
+    emailInput.classList.add("email-error");
+  }
 });
 
 dismissBtn.addEventListener("click", () => {
   content.style.display = "flex";
-  sucessMsg.style.display = "none";
+  successMsg.style.display = "none";
   form.reset();
 });
